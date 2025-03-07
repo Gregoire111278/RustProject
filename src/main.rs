9f6d687b59af97a04207e7e8cd9a5bb0a79c8082
Gregoire111278 <82652774+Gregoire111278@users.noreply.github.com>
2025-03-07 12:40:52 +0100

    commit map fin cours 07/03

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ mod map;
 
 fn main() {
     println!("Hello, world!");
+    let map = map::Map::new(100, 100, 0);
+    map.display();
 }
--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,7 @@
+use noise::{NoiseFn, Perlin};
+use rand::Rng;
+
+
 pub struct Map {
     width: usize,
     height: usize,
@@ -15,7 +19,57 @@ pub enum Tile {
 
 impl Map {
     pub fn new(width: usize, height: usize, seed: usize) -> Self {
+        let perlin = Perlin::new();
         let mut rng = rand::thread_rng();
+        let data = (0..height)
+            .map(|y| {
+                (0..width)
+                    .map(|x| {
+                        let value = perlin.get([x as f64 / 10.0, y as f64 / 10.0, seed as f64]);
+                        if value > 0.7 {
+                            Tile::Obstacle
+                        } else if value > 0.5 {
+                            Tile::Energy
+                        } else if value > 0.3 {
+                            Tile::Mineral
+                        } else if value > 0.1 {
+                            Tile::Science
+                        } else {
+                            Tile::Empty
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
 
+        Map {
+            width,
+            height,
+            data,
+        }
     }
-}
+
+    pub fn display(&self) {
+        for row in &self.data {
+            for &tile in row {
+                let symbol = match tile {
+                    Tile::Empty => ".",
+                    Tile::Obstacle => "🚧",
+                    Tile::Energy => "E",
+                    Tile::Mineral => "M",
+                    Tile::Science => "S",
+                };
+                print!("{}", symbol);
+            }
+            println!();
+        }
+    }
+    pub fn get_tile(&self, x: usize, y: usize) -> Tile {
+            self.data[y][x]
+        }
+
+        pub fn set_tile(&mut self, x: usize, y: usize, tile: Tile) {
+            self.data[y][x] = tile;
+        }
+    }
+
